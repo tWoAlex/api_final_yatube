9300d41056ed7df2dd1ca7f7b2b822f7c2cc5146
tWoAlex <thewoalex@gmail.com>
2023-03-15 01:06:02 +0500

    Second review

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
-from rest_framework.validators import UniqueTogetherValidator
 
 from posts.models import Group, Post, Comment, Follow, User
 
